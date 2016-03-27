cc544735b03ac0d80720e815e97a7553a6572f26
NataliyaD-in6k <NataliyaD@softwareplanet.uk.com>
2016-03-27 20:00:28 +0300

    add heroku configuration

---
+++ src/main/java/launch/TomcatLaunch.java
@@ -0,0 +1,28 @@
+package launch;
+
+import org.apache.catalina.WebResourceRoot;
+import org.apache.catalina.core.StandardContext;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.webresources.StandardRoot;
+
+import java.io.File;
+
+public class TomcatLaunch {
+    public static void main(String[] args) throws Exception {
+        Tomcat tomcat = new Tomcat();
+
+        String webPort = System.getenv("PORT");
+        if(webPort == null || webPort.isEmpty()) {
+            webPort = "8080";
+        }
+        tomcat.setPort(Integer.valueOf(webPort));
+
+        StandardContext ctx = (StandardContext) tomcat.addWebapp("/", new File("src/main/webapp/").getAbsolutePath());
+
+        WebResourceRoot resources = new StandardRoot(ctx);
+        ctx.setResources(resources);
+
+        tomcat.start();
+        tomcat.getServer().await();
+    }
+}
